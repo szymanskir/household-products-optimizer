91203943eab1eb78deb961bc80ac9d58a7be75e5
szymanskir <ryszard.szymanski@outlook.com>
2020-06-30 23:56:53 +0200

    Add household products data

---
+++ main.py
@@ -0,0 +1,10 @@
+from src.HouseholdProduct import HouseholdProduct
+
+ALL_PRODUCTS = {
+    HouseholdProduct(name="Fridge", price=2499),
+    HouseholdProduct(name="Dish Washer", price=1599),
+    HouseholdProduct(name="Washing Machine", price=1399),
+    HouseholdProduct(name="Induction cooker", price=1649),
+    HouseholdProduct(name="Canopy", price=499),
+    HouseholdProduct(name="Oven", price=1699)
+}
